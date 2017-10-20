889941b41b41b52ffe86abe9b928af78bb4852f8
Amiel Martin <amiel@actionsprout.com>
2017-10-19 21:30:07 -0700

    Add neo4j; disable sidekiq connect to ferns db manually

--- Gemfile
+++ Gemfile
@@ -5,33 +5,29 @@ gem 'rails', '~> 5.0.4'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
 
+gem 'neo4j'
+
 # Tools
-gem 'bugsnag'
-gem 'sidekiq', '~> 4'
-gem 'sidekiq-pro', source: "https://2aa2eab4:95d80c19@enterprise.contribsys.com/"
-gem 'sidekiq-ent', source: "https://2aa2eab4:95d80c19@enterprise.contribsys.com/"
-
-# We need sinatra for Sidekiq's UI.
-# We are using Github (master) version because we're on Rails 5.
-# REF => https://github.com/mperham/sidekiq/issues/2839#issuecomment-191723849
-gem 'sinatra', github: 'sinatra', :require => nil
-
+# gem 'bugsnag'
+# gem 'sidekiq'
+# gem 'sidekiq-pro', source: "https://2aa2eab4:95d80c19@enterprise.contribsys.com/"
+# gem 'sidekiq-ent', source: "https://2aa2eab4:95d80c19@enterprise.contribsys.com/"
 
 group :development, :test do
   gem 'dotenv-rails'
-  gem 'guard'
-  gem 'guard-rspec'
-  gem 'rspec-rails'
-  gem 'spring-commands-rspec'
+  # gem 'guard'
+  # gem 'guard-rspec'
+  # gem 'rspec-rails'
+  # gem 'spring-commands-rspec'
   gem 'pry-rails'
-  gem 'rb-fsevent', require: false
+  # gem 'rb-fsevent', require: false
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platform: :mri
+  # gem 'byebug', platform: :mri
 end
 
 group :development do
   gem 'foreman'
-  gem 'listen', '~> 3.0.5'
+  # gem 'listen', '~> 3.0.5'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,42 +1,42 @@
-require 'sidekiq/pro/web'
-
-unless ENV['SIDEKIQ_VERBOSE'].present?
-  Sidekiq::Logging.logger.level = Logger::WARN
-end
-
-# Add Basic Auth for Sidekiq UI
-sidekiq_username = ENV['SIDEKIQ_USERNAME']
-sidekiq_password = ENV['SIDEKIQ_PASSWORD']
-
-if sidekiq_username.present? && sidekiq_password.present?
-  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    # Protect against timing attacks:
-    # - See https://codahale.com/a-lesson-in-timing-attacks/
-    # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
-    # - Use & (do not use &&) so that it doesn't short circuit.
-    # - Use digests to stop length information leaking
-    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(sidekiq_username)) &
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(sidekiq_password))
-  end
-end
-
-# Sidekiq Reliability
-# https://github.com/mperham/sidekiq/wiki/Reliability
-Sidekiq::Client.reliable_push! unless Rails.env.test?
-Sidekiq.configure_server do |config|
-  # TODO: When super_fetch become mature we can switch to that algorithm.
-  # config.super_fetch!
-  config.timed_fetch!
-
-  config.reliable_scheduler!
-
-  if ENV['RUN_PERIODIC_JOBS'].present?
-    # Ent Periodic Jobs (https://github.com/mperham/sidekiq/wiki/Ent-Periodic-Jobs)
-    # Note: The Rails App is configured for Facebook Timezone but CRON jobs and server
-    #       are still in UTC.
-    config.periodic do |mgr|
-      # Example: Run everyday at 02:00 Pacific Time.
-      # mgr.register '0 10 * * *', CreateAllMissingPageMetricsJob
-    end
-  end
-end
+# require 'sidekiq/pro/web'
+#
+# unless ENV['SIDEKIQ_VERBOSE'].present?
+#   Sidekiq::Logging.logger.level = Logger::WARN
+# end
+#
+# # Add Basic Auth for Sidekiq UI
+# sidekiq_username = ENV['SIDEKIQ_USERNAME']
+# sidekiq_password = ENV['SIDEKIQ_PASSWORD']
+#
+# if sidekiq_username.present? && sidekiq_password.present?
+#   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+#     # Protect against timing attacks:
+#     # - See https://codahale.com/a-lesson-in-timing-attacks/
+#     # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+#     # - Use & (do not use &&) so that it doesn't short circuit.
+#     # - Use digests to stop length information leaking
+#     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(sidekiq_username)) &
+#       ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(sidekiq_password))
+#   end
+# end
+#
+# # Sidekiq Reliability
+# # https://github.com/mperham/sidekiq/wiki/Reliability
+# Sidekiq::Client.reliable_push! unless Rails.env.test?
+# Sidekiq.configure_server do |config|
+#   # TODO: When super_fetch become mature we can switch to that algorithm.
+#   # config.super_fetch!
+#   config.timed_fetch!
+#
+#   config.reliable_scheduler!
+#
+#   if ENV['RUN_PERIODIC_JOBS'].present?
+#     # Ent Periodic Jobs (https://github.com/mperham/sidekiq/wiki/Ent-Periodic-Jobs)
+#     # Note: The Rails App is configured for Facebook Timezone but CRON jobs and server
+#     #       are still in UTC.
+#     config.periodic do |mgr|
+#       # Example: Run everyday at 02:00 Pacific Time.
+#       # mgr.register '0 10 * * *', CreateAllMissingPageMetricsJob
+#     end
+#   end
+# end
